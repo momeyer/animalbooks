84f7a3f112d45236d18deb2196ddecbe4a38bde7
Monique <meyer.monique93@gmail.com>
2020-06-26 20:56:09 +0200

    Fixing goodreads requests

--- application.py
+++ application.py
@@ -4,12 +4,13 @@ from flask import Flask, render_template, jsonify, request, flash, session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_session import Session
+import requests
+
 
 
 app = Flask(__name__)
 
 app.secret_key = os.urandom(12)
-
 
 # DB example
 # export DATABASE_URL = "postgres://user:password@address:port/database"
@@ -17,6 +18,11 @@ app.secret_key = os.urandom(12)
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
+
+if not os.getenv("GOODREADS_KEY"):
+    raise RuntimeError("GOODREADS_KEY  is not set")
+
+GOODREADS_KEY = os.getenv("GOODREADS_KEY")
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
@@ -47,20 +53,34 @@ def registration_form_info_is_correct(name, lastname, username, password, passwo
     
     return True
 
-def generate_rate_dict(books_list):
-    rate_dict = {}
 
-    for book in books_list:
-        rate = Database.calculate_avg_rating(book.id)
-        if rate != None:
-            half_star = float(rate)%1
-            if half_star > 0:
-                rate_dict[book.id] = (int(rate), 1)
-            else:
-                rate_dict[book.id] = (int(rate), 0)
+def get_goodreads_reviews(book_isbn):
+    goodreads_info = {"goodreads_avg_review": 0,
+                      "goodreads_num_of_reviews": 0
+                    }
+
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",params={"key": GOODREADS_KEY, "isbns": book_isbn})
+    if res.status_code != 404:
+        response = res.json()["books"][0]    
+        goodreads_info["goodreads_avg_review"] = float(response["average_rating"])
+        goodreads_info["goodreads_num_of_reviews"] = int(response["work_ratings_count"])
+
+    return goodreads_info
+
+def generate_rate_dict(book):
+    rate_dict = {}
+    goodreads_info = get_goodreads_reviews(book.isbn)
+
+    rate = Database.calculate_avg_rating(book.id, goodreads_info)
+    if rate != None:
+        half_star = float(rate)%1
+        if half_star > 0:
+            rate_dict[book.id] = (int(rate), 1)
         else:
-            rate_dict[book.id] = (0,0)
-            
+            rate_dict[book.id] = (int(rate), 0)
+    else:
+        rate_dict[book.id] = (0,0)
+        
     return rate_dict
 
 
@@ -108,10 +128,6 @@ class Database():
         return db.execute(f"SELECT * FROM users WHERE id='{user_id}'").fetchone()
 
     @staticmethod
-    def select_best_books(avg_review=4.5):
-        return db.execute(f"select * from books where goodreads_avg_review>{avg_review} order by goodreads_avg_review desc;").fetchall()
-
-    @staticmethod
     def select_all_books():
         return db.execute("select * from books order by title").fetchall()
     
@@ -132,22 +148,40 @@ class Database():
 
     @staticmethod
     def select_book_by_isbn(book_isbn):
-        print("search by book_isbn", book_isbn)
-        return db.execute(f"SELECT * FROM books WHERE isbn= :isbn", {"isbn": book_isbn}).fetchone()
+        result = db.execute(f"SELECT * FROM books WHERE isbn= :isbn", {"isbn": book_isbn}).fetchone()
+
+        goodreads_info = {"goodreads_avg_review": 0,
+                          "goodreads_num_of_reviews": 0
+                          }
+
+        if result != None:
+            goodreads_info = get_goodreads_reviews(book_isbn)
+
+        return result, goodreads_info
 
     @staticmethod
     def select_review_by_user_id_and_book_id(user_id, book_id):
         return db.execute(f"select * from reviews where book_id ={book_id} and user_id={user_id}").rowcount > 0
 
+    @staticmethod
+    def calculate_avg_rating(book_id, goodreads_info):
+        local_rate = db.execute(f"SELECT AVG(rate) FROM reviews WHERE book_id={book_id};").fetchone()[0]
+        local_count = db.execute(f"SELECT * FROM reviews WHERE book_id={book_id};").rowcount
+        goodreads_avg_review = goodreads_info["goodreads_avg_review"]
+        goodreads_num_reviews = goodreads_info["goodreads_num_of_reviews"]
+        if local_rate != None:
+            rate = float(local_rate)
+        else:
+            rate = 0
+
+        avg_rating = (rate * local_count + goodreads_avg_review * goodreads_num_reviews) / (local_count + goodreads_num_reviews)
+
+
+        return round(avg_rating, 2)
 
     @staticmethod
-    def calculate_avg_rating(book_id):
-        rate = db.execute(f"SELECT AVG(rate) FROM reviews WHERE book_id={book_id};").fetchone()[0]
-        return rate
-    
-    @staticmethod
-    def get_review_count(book_id):
-        return db.execute(f"SELECT COUNT(*) FROM reviews WHERE book_id={book_id}").fetchone()[0]
+    def get_review_count(book_id, goodreads_info):
+        return goodreads_info["goodreads_num_of_reviews"] + db.execute(f"SELECT COUNT(*) FROM reviews WHERE book_id={book_id}").fetchone()[0]
    
 
 @app.route("/")
@@ -182,10 +216,9 @@ def login():
         if password == user_info.pw:
 
             session['user_id'] = user_info.id
-            books_list = Database.select_best_books()
-            rate_dict = generate_rate_dict(books_list)
+            books_list = Database.select_all_books()
             
-            return render_template("home.html", books_list=books_list, rate_dict=rate_dict, pagetitle="Best Books")
+            return render_template("home.html", books_list=books_list, pagetitle="All Books")
         else:
             return render_template("error.html", message="Wrong password.")    
     else:    
@@ -219,33 +252,31 @@ def search():
         pagetitle = "Sorry, book not found"
     else:
         pagetitle = f" Result for '{search_content}'"
-    
-    rate_dict = generate_rate_dict(result)
 
-    return render_template("home.html", books_list=result, rate_dict=rate_dict, pagetitle=pagetitle)
+    return render_template("home.html", books_list=result, pagetitle=pagetitle)
 
 
 @app.route("/all-books")
 def all_books():
     all_books_list = Database.select_all_books()
-   
-    rate_dict = generate_rate_dict(all_books_list)
-
-    return render_template("home.html", books_list=all_books_list, rate_dict=rate_dict, pagetitle="All Books A-Z")
+  
+    return render_template("home.html", books_list=all_books_list, pagetitle="All Books A-Z")
 
 @app.route("/all-books/<int:book_id>")
 def more(book_id):
 
     book = Database.select_book_by_id(book_id)
-    
+
     if book is None:
         return render_template("error.html", message="book not found, sorry")
     else:
         reviews_list = Database.select_reviews(book_id)
         user_info = Database.get_users_info(reviews_list)
+        rate_dict = generate_rate_dict(book)
+       
+        goodreads_info = get_goodreads_reviews(book.isbn)
         
-
-        return render_template("book.html", book=book, reviews_list=reviews_list, user_info=user_info, pagetitle="Reviews from others")
+        return render_template("book.html", book=book, reviews_list=reviews_list, goodreads_num_of_reviews=goodreads_info["goodreads_num_of_reviews"],  goodreads_avg_review=goodreads_info["goodreads_avg_review"],  user_info=user_info, rate_dict=rate_dict, pagetitle="Reviews from others")
 
 
 @app.route("/submited/<int:book_id>", methods=["POST"])
@@ -265,20 +296,19 @@ def submit_review(book_id):
     else:
         Database.insert_into_reviews(session['user_id'], review, rate, book_id)
         all_books_list = Database.select_all_books()
-        rate_dict = generate_rate_dict(all_books_list)
 
-        return render_template("home.html", books_list=all_books_list, rate_dict=rate_dict, pagetitle="All Books A-Z")
+        return render_template("home.html", books_list=all_books_list, pagetitle="All Books A-Z")
 
 @app.route("/api/<book_isbn>")
 def book_api(book_isbn):
 
-    book = Database.select_book_by_isbn(book_isbn)
+    book, goodreads_info = Database.select_book_by_isbn(book_isbn)
   
     if book is None:
-        return jsonify({"error":"invalid isbn"}), 422
+        return jsonify({"error":"invalid isbn"}), 404
 
     review_count = Database.get_review_count(book.id)
-    average_score = int(Database.calculate_avg_rating(book.id))
+    average_score = Database.calculate_avg_rating(book.id, goodreads_info)
 
     return jsonify({
             "title": book.title,
